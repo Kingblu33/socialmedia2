f5fb387f5e0750e5e9579e7d0c2851664a6cda9a
Kingblu33 <Demmarblue@gmail.com>
2022-10-26 00:50:29 -0400

    fixed blurry images

--- src/components/MasonryLayout.js
+++ src/components/MasonryLayout.js
@@ -4,16 +4,16 @@ import Pin from './Pin'
 
 const breakpointObj = {
     default: 5,
-    3000: 5,
-    2000: 4,
-    1200: 3,
+    3000: 4,
+    2000: 3,
+    1200: 2,
     1000: 2,
     500: 1,
 }
 const MasontryLayout = ({ pins }) => {
     return (
         <Masonry className='flex animate-slide-fwd' breakpointCols={breakpointObj}>
-            {pins?.map((pin) => <Pin key={pin._id} pin={pin} className="w-max" />)}
+            {pins?.map((pin) => <Pin key={pin._id} pin={pin} className="w-max " />)}
         </Masonry>
     )
 }
